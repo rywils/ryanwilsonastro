2adceeb0839a174f8d2d315e0b4db08b40441ee3
Ryan Wilson <42699070+rywils@users.noreply.github.com>
2025-09-29 07:19:15 -0500

    Initial Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,14 +8,29 @@ import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 
 
+import maintenance from "astro-maintenance";
+
+
 // https://astro.build/config
 export default defineConfig({
   
   site: "https://ryanwilson.io",
+
      vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(), 
+    sitemap(), 
+  maintenance({
+      enabled: true, // Maintenance mode enabled
+      template: "simple", // Options: 'simple', 'countdown', 'construction' or imported template content
+      title: "Site Under Maintenance",
+      description: "We are performing scheduled maintenance. Please check back soon.",
+      override: "preview", // Access site with ?preview
+      // Other optional parameters...
+    }),
+  ],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
